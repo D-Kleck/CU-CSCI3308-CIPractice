32b14d71511328043b534a38261b462d2bb063bc
D-Kleck <david.kleckner@gmail.com>
2016-04-19 11:32:20 -0600

    Update geometry_test.c
    Part 3 of Lab14

--- geometry_test.c
+++ geometry_test.c
@@ -172,6 +172,23 @@ Suite* coord_2d_suite(void)
 
 }
 
+START_TEST(test_2d_area_triangle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+
+    a.x = 0;
+    a.y = 0;
+    b.x = 0;
+    b.y = 0;
+    c.x = 0;
+    c.y = 0;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0);
+}
+END_TEST
+
+
 /* main: run test suites and set exit status */
 int main(void){
 
